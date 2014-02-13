1785555a7f66eb9397b618af0ab26a29c0d36795
donovan6000 <donovancudney@yahoo.com>
2014-02-12 20:44:41 -0800

    Fixed version number

--- main.cpp
+++ main.cpp
@@ -110,7 +110,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 
 	// Create window
 	hwnd = CreateWindow(appName,
-		TEXT("Universal IFR Extractor v0.3"),
+		TEXT("Universal IFR Extractor v0.4"),
 		WS_SYSMENU | WS_MINIMIZEBOX,
 		0, 0,
 		354, 135,
